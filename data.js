9c389d76676bcadef05319a7e68deb25f5ebef97
sagethemeowwish <126026287+sagethemeowwish@users.noreply.github.com>
2025-10-02 13:35:12 -0400

    test

---
+++ data.js
@@ -0,0 +1,39 @@
+// Your personal information/content
+
+const siteData = {
+  musicURL: "https://luna.ponyvillefm.com/listen/lunaradio/radio.mp3",
+
+  socialLinks: [
+    { name: "X", url: "https://twitter.com/", tooltip: "Twitter (X)" },
+    { name: "Bluesky", url: "https://bsky.app/", tooltip: "Bluesky" },
+    { name: "VRChat", url: "https://hello.vrchat.com/", tooltip: "VRChat" },
+    { name: "Spotify", url: "https://spotify.com/", tooltip: "Spotify" },
+    { name: "Fediverse", url: "https://mastodon.social/", tooltip: "Fediverse" },
+    { name: "YouTube", url: "https://youtube.com/", tooltip: "YouTube" }
+  ],
+
+  about: {
+    title: "About Me",
+    text: "I’m sagethenova — Gamer, furry, brony, and tech enthusiast. I play VRChat, COD, 7 Days to Die, and more!",
+    games: [
+      "VRChat", "Call of Duty Cold War", "Palworld", "Terraria",
+      "Wild Assault", "Atlas", "Forewarned", "Call of Duty Black Ops 3",
+      "Cattails", "Gunfire Reborn", "Devour", "Lethal Company",
+      "Star Trek Bridge Crew", "Ironwolf VR", "Indigo Park", "War Trigger 1, 2, 3",
+      "7 Days to Die (favorite)", "Resonate"
+    ]
+  },
+
+  avatars: [
+    { name: "Nova Beast", img: "v (1).png", url: "#" },
+    { name: "Avatar 2", img: "v (2).png", url: "#" },
+    { name: "Avatar 3", img: "v (3).png", url: "#" },
+    { name: "Avatar 4", img: "v (4).png", url: "#" }
+  ],
+
+  contact: {
+    discord: "sagethenova",
+    steam: "988123941",
+    xbox: "sagethecat"
+  }
+};
